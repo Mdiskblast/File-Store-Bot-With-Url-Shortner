0f3a576f863f87b3f4388ac5090e6aedd68298d0
Mdiskblast <154430141+Mdiskblast@users.noreply.github.com>
2023-12-20 17:34:21 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,17 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  API_ID = int(os.environ.get("API_ID", "29732337"))
+  API_HASH = os.environ.get("API_HASH", "27c900a8bac51da6d0fd91aad09ef779")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6561956588:AAFElWnnqYYrgamjMlY3NkFGR_L3RkXb_hU")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "MBfilestoreBot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "2014221486"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', ")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "5790619836"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://dokefiv235:ZHnCNX9nbViXFMuZ@snapdemon1.x0iinji.mongodb.net/?retryWrites=true&w=majority")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "2038807427")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "2014221486"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -23,7 +23,7 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 
 ╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
 │
-├🔸 My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
+├🔸 My Name: [MB FileStore Bot](https://t.me/MBfilestoreBot)
 │
 ├🔸 Language: [Python 3](https://www.python.org)
 │
@@ -32,16 +32,18 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 ╰──────[ 😎 ]───────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [Jᴀcκ々sᴘᴀʀʀow™](https://telegram.me/Capta1_n)
  
  I am Super noob Please Support My Hard Work.
 
-[Donate Me](https://t.me/KingVj01)
+[Donate Me](https://t.me/Capta1_n)
 """
   HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
 
 How to Use Bot & it's Benefits??
+
+💲 Activate Bot First ✅
 
 📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
 
